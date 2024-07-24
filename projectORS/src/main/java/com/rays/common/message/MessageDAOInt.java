061fabbb75991b5ee16517a41d54aa95cc057478
ilavishjha <lavishojha2004@gmail.com>
2024-07-24 15:05:28 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Lavish Ojha 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
